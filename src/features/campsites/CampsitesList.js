ac31c04f62d2d40e1efc6d7ad34c0c02a43cb5f1
Nephi Banegas <nephi.banegas@yahoo.com>
2023-04-27 08:11:50 -0500

    useState

--- src/features/campsites/CampsitesList.js
+++ src/features/campsites/CampsitesList.js
@@ -1,13 +1,21 @@
-import { CAMPSITES } from "../../app/shared/CAMPSITES";
+
 import CampsiteCard from "./CampsiteCard";
 import {Col, Row} from 'reactstrap';
+import { selectAllCampsites } from "./campsitesSlice";
 
-const CampsitesList = () => {
+const CampsitesList = ({setCampsiteId}) => {
+    const campsites = selectAllCampsites();
     return (
         <Row className="ms-auto">
-            {CAMPSITES.map((cs) => (
-                <Col md="5" className="m-4" key={CampsiteCard.id}>
-                    <CampsiteCard campsite={cs} />
+            {campsites.map((cs, index) => (
+                <Col 
+                    md="5" 
+                    className="m-4" 
+                    key={CampsiteCard.id}
+                    onClick={() => setCampsiteId(cs.id)}
+                    >
+                    
+                    <CampsiteCard campsite={cs} key={index}/>
                 </Col>
             ))}
         </Row>
