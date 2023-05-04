fd91507a507726c88031fe77ecf56e9e5dad2e4b
Nephi Banegas <nephi.banegas@yahoo.com>
2023-05-03 20:17:19 -0500

    Contact Form 1-3

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="contact" element={<ContactPage />} />
-        <Route path="directory" element={<CampsitesDirectoryPage />} />
+        
         <Route path="about" element={<AboutPage />} />
         <Route path="directory/:campsiteId" element={<CampsiteDetailPage />} />
       </Routes>
